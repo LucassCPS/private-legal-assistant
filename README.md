99166df5055946a5c883a30cf57f9780da412504
LucassCPS <lucas.pini@hotmail.com>
2025-04-15 14:02:33 -0300

    Add initial project files and references

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # private-legal-assistant
+
+Sistema que implementa um assistente virtual para orientação jurídica sobre o registro civil garantindo a privacidade do usuário.
+
+Primeiramente instale o poetry com o comando: 
+`curl -sSL https://install.python-poetry.org | python3 -`
+
+Instale as dependencias com:
+`poetry install`
+
+Baixe e instale o Ollama:
+`curl -fsSL https://ollama.com/install.sh | sh`
+
+Em um terminal separado, inicie o ollama server através do comando:
+`ollama serve`
+
+Em um outro terminal rode o modelo:
+`ollama run llama3.2:3b-instruct-fp16 --keepalive -1m`
+
+Execute o programa com:
+`poetry run python3 legal-assistant`
---
+++ legal_assistant/__main__.py
@@ -0,0 +1,29 @@
+# https://medium.com/@lucasmassucci/entity-recognition-with-llms-and-the-importance-of-prompt-engineering-all-languages-ceda8a7ff3e2
+# https://github.com/meta-llama/llama-stack
+# https://github.com/meta-llama/llama-stack/tree/main/docs/zero_to_hero_guide#setup-ollama
+# https://github.com/chroma-core/chroma
+
+import os
+from llama_stack_client import LlamaStackClient
+
+# Get the model ID from the environment variable
+INFERENCE_MODEL = os.environ.get("INFERENCE_MODEL")
+
+# Check if the environment variable is se
+if INFERENCE_MODEL is None:
+    raise ValueError("The environment variable 'INFERENCE_MODEL' is not set.")
+
+# Initialize the client
+client = LlamaStackClient(base_url="http://localhost:8321")
+
+# Create a chat completion reques
+response = client.inference.chat_completion(
+    messages=[
+        {"role": "system", "content": "You are a friendly assistant."},
+        {"role": "user", "content": "Write a two-sentence poem about llama."},
+    ],
+    model_id=INFERENCE_MODEL,
+)
+
+# Print the response
+print(response.completion_message.content)
